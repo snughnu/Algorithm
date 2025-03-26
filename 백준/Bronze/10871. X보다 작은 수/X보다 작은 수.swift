e6b20a95a8996bdc9bd2b44ecb4087c6c93cc575
snughnu <ksnughnu@gmail.com>
2025-03-26 15:06:37 +0900

    [Bronze V] Title: X보다 작은 수, Time: 12 ms, Memory: 70012 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.swift
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.swift
@@ -1,14 +1,8 @@
-func solution(_ n: Int, _ target: Int, _ nums: [Int]) {
+func solution(_ target: Int, _ nums: [Int]) {
     let result = nums.filter { $0 < target }
     print(result.map { String($0) }.joined(separator: " "))
 }
 
-if let firstLine = readLine(),
-   let secondLine = readLine() {
-    let parts = firstLine.split(separator: " ").map { Int($0)! }
-    let n = parts[0]
-    let target = parts[1]
-    let nums = secondLine.split(separator: " ").map { Int($0)! }
-    solution(n, target, nums)
-}
-
+let target = readLine()!.split(separator: " ").map { Int($0)! }[1]
+let nums = readLine()!.split(separator: " ").map { Int($0)! }
+solution(target, nums)
