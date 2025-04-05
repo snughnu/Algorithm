1bf093280519a785fc8b5a938db4f0829473bad0
snughnu <ksnughnu@gmail.com>
2025-04-05 14:29:01 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -0,0 +1,11 @@
+func solution(_ s: String) -> Bool {
+    if s.count == 4 || s.count == 6 {
+        if !(Int(s) == nil) {
+            return true
+        } else {
+            return false
+        }        
+    } else {
+        return false
+    }
+}
