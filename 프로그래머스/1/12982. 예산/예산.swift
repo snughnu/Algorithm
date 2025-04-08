bbe749cdb7221cebc9b9d90256965a6510acbcbf
snughnu <ksnughnu@gmail.com>
2025-04-08 12:52:19 +0900

    [level 1] Title: 예산, Time: 0.63 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.swift
+++ 프로그래머스/1/12982. 예산/예산.swift
@@ -1,26 +1,11 @@
-import Foundation
-
-func solution(_ d:[Int], _ budget:Int) -> Int {
-    
-    //d를 오름차순으로 정렬하는 알고리즘
-    let sortedD = d.sorted()
-    
-    //몇 개의 부서인지 헤아리는 알고리즘
-        //변수 설정
-            //변수: 반복상수, 합계, 몇 개의 부서인지
-    var count: Int = 0
-    var sum: Int = 0
-    var i: Int = 0
-    
-        //while문으로
-            //조건: 합계 <= 예산
-            //합계 += 정렬된 배열[i]
-            //몇 개의 부서 += 1
-    while i < sortedD.count && sum + sortedD[i] <= budget {
-        sum += sortedD[i]
-        i += 1
-        count += 1
+func solution(_ d: [Int], _ budget: Int) -> Int {
+    let nums = d.sorted()
+    var sum = 0
+    var count = 0
+    for i in (0..<nums.count) {
+        sum += nums[i]
+        if sum > budget { return i }
+        count = i
     }
-    
-    return count
+    return count+1
 }
